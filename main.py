e8029222ea864f080c9c4d662cbd1d29380618a2
Glytchd <goodwin78ak@gmail.com>
2025-03-08 17:36:09 -0900

    Finished lesson commit

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import sys
 from stats import get_num_words, get_num_char, srt_dict
+
+if len(sys.argv) < 2:
+     print('Usage: python3 main.py <path_to_book>')
+     sys.exit(1)
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_num_words(text)
     char_dict = get_num_char(text)
